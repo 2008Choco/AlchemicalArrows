7fdc403942a5a7ca0e7651af65380b83ee7fc5d0
Parker Hawke <hawkeboyz2@hotmail.com>
2019-04-30 22:01:21 -0400

    Use the ArrowStateManager in ArrowPickupListener

--- src/main/java/wtf/choco/arrows/listeners/PickupArrowListener.java
+++ src/main/java/wtf/choco/arrows/listeners/PickupArrowListener.java
@@ -8,21 +8,21 @@ import org.jetbrains.annotations.NotNull;
 
 import wtf.choco.arrows.AlchemicalArrows;
 import wtf.choco.arrows.api.AlchemicalArrowEntity;
-import wtf.choco.arrows.registry.ArrowRegistry;
+import wtf.choco.arrows.registry.ArrowStateManager;
 
 public final class PickupArrowListener implements Listener {
 
-    private final ArrowRegistry arrowRegistry;
+    private final ArrowStateManager stateManager;
 
     public PickupArrowListener(@NotNull AlchemicalArrows plugin) {
-        this.arrowRegistry = plugin.getArrowRegistry();
+    	this.stateManager = plugin.getArrowStateManager();
     }
 
     @EventHandler
     @SuppressWarnings("deprecation")
     public void onPickupArrow(PlayerPickupArrowEvent event) {
         Arrow arrow = event.getArrow();
-        AlchemicalArrowEntity alchemicalArrow = arrowRegistry.getAlchemicalArrow(arrow);
+        AlchemicalArrowEntity alchemicalArrow = stateManager.get(arrow);
         if (alchemicalArrow == null) return;
 
         event.getItem().setItemStack(alchemicalArrow.getImplementation().getItem());
