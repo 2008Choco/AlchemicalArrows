80c156e921437fa40fd3ebd530d7bdc8cc49d16a
Parker Hawke <hawkeboyz2@hotmail.com>
2020-08-06 21:41:14 -0400

    FINALLY add support for shooting alchemical arrows from dispensers

--- src/main/java/wtf/choco/arrows/listeners/ProjectileShootListener.java
+++ src/main/java/wtf/choco/arrows/listeners/ProjectileShootListener.java
@@ -1,12 +1,15 @@
 package wtf.choco.arrows.listeners;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import com.google.common.collect.Iterables;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.AbstractArrow.PickupStatus;
 import org.bukkit.entity.Arrow;
@@ -14,9 +17,14 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.projectiles.BlockProjectileSource;
+import org.bukkit.projectiles.ProjectileSource;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -29,6 +37,8 @@ import wtf.choco.arrows.api.property.ArrowProperty;
 public final class ProjectileShootListener implements Listener {
 
     private static final Random RANDOM = new Random();
+
+    private final Map<Block, AlchemicalArrow> recentlyDispensed = new HashMap<>();
 
     private final AlchemicalArrows plugin;
 
@@ -88,6 +98,45 @@ public final class ProjectileShootListener implements Listener {
         }
 
         AlchemicalArrowShootEvent aasEvent = new AlchemicalArrowShootEvent(alchemicalArrowEntity, shooter);
+        Bukkit.getPluginManager().callEvent(aasEvent);
+        if (aasEvent.isCancelled()) {
+            event.setCancelled(true);
+            return;
+        }
+
+        this.plugin.getArrowStateManager().add(alchemicalArrowEntity);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onDispenserShootAlchemicalArrow(BlockDispenseEvent event) {
+        AlchemicalArrow alchemicalArrow = plugin.getArrowRegistry().get(event.getItem());
+        if (alchemicalArrow == null) {
+            return;
+        }
+
+        this.recentlyDispensed.put(event.getBlock(), alchemicalArrow);
+    }
+
+    @EventHandler
+    public void onDispenserShootAlchemicalArrow(ProjectileLaunchEvent event) {
+        ProjectileSource source = event.getEntity().getShooter();
+        if (!(source instanceof BlockProjectileSource) || !(event.getEntity() instanceof Arrow)) {
+            return;
+        }
+
+        BlockProjectileSource blockSource = (BlockProjectileSource) source;
+        AlchemicalArrow alchemicalArrow = recentlyDispensed.remove(blockSource.getBlock());
+        if (alchemicalArrow == null) {
+            return;
+        }
+
+        AlchemicalArrowEntity alchemicalArrowEntity = alchemicalArrow.createNewArrow((Arrow) event.getEntity());
+        if (!alchemicalArrow.onShootFromBlockSource(alchemicalArrowEntity, blockSource)) {
+            event.setCancelled(true);
+            return;
+        }
+
+        AlchemicalArrowShootEvent aasEvent = new AlchemicalArrowShootEvent(alchemicalArrowEntity, blockSource);
         Bukkit.getPluginManager().callEvent(aasEvent);
         if (aasEvent.isCancelled()) {
             event.setCancelled(true);
