e2acae7fab3f079790758b13e201a853ca106366
Parker Hawke <hawkeboyz2@hotmail.com>
2021-01-08 17:52:06 -0500

    Fix WorldGuard override permission and cleanup WorldGuard support

--- src/main/java/wtf/choco/arrows/listeners/ArrowHitPlayerListener.java
+++ src/main/java/wtf/choco/arrows/listeners/ArrowHitPlayerListener.java
@@ -1,11 +1,11 @@
 package wtf.choco.arrows.listeners;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
 import com.sk89q.worldguard.protection.flags.Flags;
-import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 
 import org.bukkit.entity.Arrow;
@@ -33,11 +33,11 @@ public final class ArrowHitPlayerListener implements Listener {
             return;
         }
 
-        Player player = (Player) event.getEntity();
+        Player damaged = (Player) event.getEntity();
         Arrow arrow = (Arrow) event.getDamager();
         AlchemicalArrowEntity alchemicalArrow = plugin.getArrowStateManager().get(arrow);
 
-        if (alchemicalArrow == null || player.isBlocking()) {
+        if (alchemicalArrow == null) {
             return;
         }
 
@@ -49,34 +49,30 @@ public final class ArrowHitPlayerListener implements Listener {
             RegionQuery query = worldguard.getRegionContainer().createQuery();
 
             // Check state of shooter
-            if (arrow.getShooter() instanceof Player) {
+            if (source instanceof Player && !((Player) source).hasPermission("arrows.worldguardoverride")) {
                 Player shooter = (Player) source;
-                if (!shooter.hasPermission("arrows.worldguardoverride")) {
-                    StateFlag.State state = query.queryState(BukkitAdapter.adapt(shooter.getLocation()), worldguardPlugin.wrapPlayer(shooter), Flags.PVP);
+                LocalPlayer localShooter = worldguardPlugin.wrapPlayer(shooter);
 
-                    if (!shooter.hasPermission("arrows.worldguardoverride") && state == StateFlag.State.DENY) {
-                        shooter.sendMessage(AlchemicalArrows.CHAT_PREFIX + "You cannot hit a player whilst protected by PvP");
-                        event.setCancelled(true);
-                        return;
-                    }
-                }
-            }
-
-            // Check state of damaged
-            if (event.isCancelled() || !query.testState(BukkitAdapter.adapt(player.getLocation()), null, Flags.PVP)) {
-                if (arrow.getShooter() instanceof Player) {
-                    ((Player) source).sendMessage(AlchemicalArrows.CHAT_PREFIX + "This player is protected from PvP");
+                // Check state of shooter
+                if (!query.testState(BukkitAdapter.adapt(shooter.getLocation()), localShooter, Flags.PVP)) {
+                    shooter.sendMessage(AlchemicalArrows.CHAT_PREFIX + "You cannot hit a player whilst protected by PvP");
+                    event.setCancelled(true);
+                    return;
                 }
 
-                event.setCancelled(true);
-                return;
+                // Check state of damaged
+                if (event.isCancelled() || !query.testState(BukkitAdapter.adapt(damaged.getLocation()), localShooter, Flags.PVP)) {
+                    shooter.sendMessage(AlchemicalArrows.CHAT_PREFIX + "This player is protected from PvP");
+                    event.setCancelled(true);
+                    return;
+                }
             }
         }
 
         // AlchemicalArrows arrow handling
         AlchemicalArrow type = alchemicalArrow.getImplementation();
         type.hitEntityEventHandler(alchemicalArrow, event);
-        type.onHitPlayer(alchemicalArrow, player);
+        type.onHitPlayer(alchemicalArrow, damaged);
     }
 
 }
