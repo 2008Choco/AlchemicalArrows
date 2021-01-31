93e864138d25ca55ded21dfe4d735a6b43ca9813
Parker Hawke <hawkeboyz2@hotmail.com>
2021-01-31 16:34:55 -0500

    Allow PVP in default flags and don't spam warning messages (2 sec delay)

--- src/main/java/wtf/choco/arrows/listeners/ArrowHitPlayerListener.java
+++ src/main/java/wtf/choco/arrows/listeners/ArrowHitPlayerListener.java
@@ -6,7 +6,13 @@ import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
 import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
@@ -20,6 +26,9 @@ import wtf.choco.arrows.api.AlchemicalArrow;
 import wtf.choco.arrows.api.AlchemicalArrowEntity;
 
 public final class ArrowHitPlayerListener implements Listener {
+
+    private static final Map<UUID, Long> LAST_WARNED = new HashMap<>();
+    private static final long WARN_TIMEOUT_MILLISECONDS = TimeUnit.SECONDS.toMillis(2);
 
     private final AlchemicalArrows plugin;
 
@@ -54,15 +63,15 @@ public final class ArrowHitPlayerListener implements Listener {
                 LocalPlayer localShooter = worldguardPlugin.wrapPlayer(shooter);
 
                 // Check state of shooter
-                if (!query.testState(BukkitAdapter.adapt(shooter.getLocation()), localShooter, Flags.PVP)) {
-                    shooter.sendMessage(AlchemicalArrows.CHAT_PREFIX + "You cannot hit a player whilst protected by PvP");
+                if (query.queryState(BukkitAdapter.adapt(shooter.getLocation()), localShooter, Flags.PVP) == StateFlag.State.DENY) {
+                    this.attemptWarn(shooter, AlchemicalArrows.CHAT_PREFIX + "You cannot hit a player whilst protected by PvP");
                     event.setCancelled(true);
                     return;
                 }
 
                 // Check state of damaged
-                if (event.isCancelled() || !query.testState(BukkitAdapter.adapt(damaged.getLocation()), localShooter, Flags.PVP)) {
-                    shooter.sendMessage(AlchemicalArrows.CHAT_PREFIX + "This player is protected from PvP");
+                if (event.isCancelled() || query.queryState(BukkitAdapter.adapt(damaged.getLocation()), localShooter, Flags.PVP) == StateFlag.State.DENY) {
+                    this.attemptWarn(shooter, AlchemicalArrows.CHAT_PREFIX + "This player is protected from PvP");
                     event.setCancelled(true);
                     return;
                 }
@@ -73,6 +82,18 @@ public final class ArrowHitPlayerListener implements Listener {
         AlchemicalArrow type = alchemicalArrow.getImplementation();
         type.hitEntityEventHandler(alchemicalArrow, event);
         type.onHitPlayer(alchemicalArrow, damaged);
+    }
+
+    private void attemptWarn(Player player, String message) {
+        long now = System.currentTimeMillis();
+        UUID playerUUID = player.getUniqueId();
+
+        if (now - LAST_WARNED.getOrDefault(playerUUID, 0L) < WARN_TIMEOUT_MILLISECONDS) {
+            return;
+        }
+
+        LAST_WARNED.put(playerUUID, now);
+        player.sendMessage(message);
     }
 
 }
